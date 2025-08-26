68fe1e0ba103c4a00a19ae77f8f3ce191cdcc4e8
Amal Kariyawasam <12756149+1Amal@users.noreply.github.com>
2025-08-26 16:38:11 +1000

    Update tailwind css

--- tailwind-tutorial/src/App.tsx
+++ tailwind-tutorial/src/App.tsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -15,11 +15,29 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+
+        <div className="mx-auto flex max-w-sm items-center gap-x-4 rounded-xl bg-white p-6 shadow-lg outline outline-black/5 dark:bg-slate-800 dark:shadow-none dark:-outline-offset-1 dark:outline-white/10">
+          <a href="https://react.dev" target="_blank">
+            <img
+              src={reactLogo}
+              className="logo react"
+              alt="React logo size-12 shrink-0 hover:bg-sky-700"
+            />
+          </a>
+          <div>
+            <div className="text-xl font-medium text-black dark:text-white hover:bg-sky-700">
+              ChitChat
+            </div>
+            <p className="text-gray-500 dark:text-gray-400">
+              You have a new message!
+            </p>
+          </div>
+        </div>
         <h1 className="font-bold underline ">This is Tailwind CSS</h1>
       </div>
       <h1 className="underline">Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button className="hover:bg-sky-1700"onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
         <p>
@@ -30,7 +48,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
